826794ad18983110bd3535c9681f23bbc97ac757
Sam Held <sheld816@gmail.com>
2023-06-28 14:53:40 -0400

    Add project files.

---
+++ ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace server
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            Console.WriteLine($"Got a Send Message from {user}");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task GetStats(string user)
+        {
+            Console.WriteLine($"Got a GetStats from {user}");
+            await Clients.All.SendAsync("ReceiveStats", 12345);
+        }
+
+        public async Task UnInstall(string user, string message)
+        {
+            Console.WriteLine($" {user} wants to uninstall program");
+            await Clients.All.SendAsync("RequestReceived", user, message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+namespace server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSignalR();
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+            app.MapHub<ChatHub>("/chatHub");
+            app.Run();
+        }
+    }
+}
